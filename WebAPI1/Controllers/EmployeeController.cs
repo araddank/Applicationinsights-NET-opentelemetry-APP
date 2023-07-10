4989bc1d2e3ea09be486f78bcea7473c39b8d65f
Aruna Addanki <araddank@microsoft.com>
2023-07-10 16:35:53 +0530

    Initial commit

---
+++ WebAPI1/Controllers/EmployeeController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPI1.Models;
+
+namespace WebAPI1.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        // GET: api/Employee
+       // public IEnumerable<string> Get()
+       // {
+       //     return new string[] { "value1", "value2" };
+       // }
+
+        // GET: api/Employee/5
+       // public IEnumerable<Employee> Get(int id)
+       // {
+       //     return "value";
+      //  }
+
+        public IEnumerable<Employee> Get()
+        {
+            using (DotNetAppSqlDb_dbEntities entities = new DotNetAppSqlDb_dbEntities())
+            {
+                return entities.Employees.ToList();
+            }
+        }
+
+        public Employee Get(int id)
+        {
+            using (DotNetAppSqlDb_dbEntities entities = new DotNetAppSqlDb_dbEntities())
+            {
+                return entities.Employees.FirstOrDefault(e => e.EmployeeId == id);
+            }
+        }
+        // POST: api/Employee
+        public void Post([FromBody]string value)
+        {
+
+        }
+
+        // PUT: api/Employee/5
+        public void Put(int id, [FromBody]string value)
+        {
+           // using (DotNetAppSqlDb_dbEntities entities = new DotNetAppSqlDb_dbEntities())
+           // {
+           //     Employee emp = new Employee();
+           //     emp.EmployeeId = id;
+           //     entities.Employees.Add(emp);
+           //     entities.SaveChanges();
+          //  }
+        }
+
+        // DELETE: api/Employee/5
+        public void Delete(int id)
+        {
+            using (DotNetAppSqlDb_dbEntities entities = new DotNetAppSqlDb_dbEntities())
+            {
+                Employee emp = entities.Employees.Find(id);
+                entities.Employees.Remove(emp);
+                entities.SaveChanges();
+            }
+        }
+    }
+}
---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            IEnumerable<MemberViewModel> members = null;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44341/api/Swagger");
+
+                //Called Member default GET All records  
+                //GetAsync to send a GET request   
+                // PutAsync to send a PUT request  
+                var responseTask = client.GetAsync("employee");
+                responseTask.Wait();
+
+                //To store result of web api response.   
+                var result = responseTask.Result;
+
+                //If success received   
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<IList<MemberViewModel>>();
+                    readTask.Wait();
+
+                    members = readTask.Result;
+                }
+                else
+                {
+                    //Error response received   
+                    members = Enumerable.Empty<MemberViewModel>();
+                    ModelState.AddModelError(string.Empty, "Server error try after some time.");
+                }
+            }
+            return View(members);
+        }
+        public ActionResult GetMembers()
+        {
+            IEnumerable<MemberViewModel> members = null;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44341/api");
+
+                //Called Member default GET All records  
+                //GetAsync to send a GET request   
+                // PutAsync to send a PUT request  
+                var responseTask = client.GetAsync("employee");
+                responseTask.Wait();
+
+                //To store result of web api response.   
+                var result = responseTask.Result;
+
+                //If success received   
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<IList<MemberViewModel>>();
+                    readTask.Wait();
+
+                    members = readTask.Result;
+                }
+                else
+                {
+                    //Error response received   
+                    members = Enumerable.Empty<MemberViewModel>();
+                    ModelState.AddModelError(string.Empty, "Server error try after some time.");
+                }
+            }
+            return View(members);
+        }
+        //var responseTask = client.GetAsync("member");
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Delete()
+        {
+            IEnumerable<MemberViewModel> members = null;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44341/api/swagger");
+
+                //Called Member default GET All records  
+                //GetAsync to send a GET request   
+                // PutAsync to send a PUT request  
+                var responseTask = client.GetAsync("employee");
+                responseTask.Wait();
+
+                //To store result of web api response.   
+                var result = responseTask.Result;
+
+                //If success received   
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<IList<MemberViewModel>>();
+                    readTask.Wait();
+
+                    members = readTask.Result;
+                }
+                else
+                {
+                    //Error response received   
+                    members = Enumerable.Empty<MemberViewModel>();
+                    ModelState.AddModelError(string.Empty, "Server error try after some time.");
+                }
+            }
+            return View(members);
+        }
+    }
+}
